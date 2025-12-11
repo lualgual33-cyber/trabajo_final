b8e04803e7f53af128cf4e7aca0f1b4c3bbe0e99
Johan Tequia <199398882+JohanSebastianTequiaForero@users.noreply.github.com>
2025-12-10 23:25:51 -0500

    subiendo

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,4 +1,4 @@
-const API_URL = "https://trabajofinal-production-4e79.up.railway.app";
+const API_URL = "https://trabajofinal-production-4e79.up.railway.app/";
 
 export async function getTodos() {
   const res = await fetch(`${API_URL}/todos`);
